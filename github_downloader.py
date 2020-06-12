9a1f11d036423601ea279d1dba8dfe2d5bb6088f
Aleksandar Rajic <rajicaleksandar6@gmail.com>
2020-06-12 13:34:42 +0200

    onrisane staticne promenjive

--- github_downloader.py
+++ github_downloader.py
@@ -6,32 +6,22 @@ browser = webdriver.Chrome()
 
 class Folder:
 
-    root = None
-    name = None
-    folders = []
-    files = []
-    href = None
-
-    stack_of_folders = []
-
     def __init__(self, rootFolder, name, href):
         self.root = rootFolder
         self.name = name
         self.href = href
-        files = []
+        self.files = []
+        self.folders = []
 
 
     def find(self):
         global brower
         browser.get(self.href)
-        #print(self.files)
-        #print('usao')
-        #print('moj root je ' + str(self.root))
-        #print('moj link je ' + str(self.href))
-        # pronalazi sve iteme u folderu
+
+
         items = browser.find_elements_by_class_name('js-navigation-open ')
 
-        #print([item.get_attribute('title') for item in items])
+
         for item in items:
 
             if item.get_attribute('href') != '' and item.text != '..':
@@ -43,20 +33,15 @@ class Folder:
 
                     self.folders.append(Folder(self.href, name, href))
 
-                    #folder.find(browser)
-
                 else:
                     href = item.get_attribute('href')
                     name = item.text
-                    # print(href)
 
                     self.files.append(File(self.href, name, href))
 
-                    #print('Fajl dodat: ' + str(href))
-
         self.print_childs()
 
-        self.stack_of_folders = self.folders.copy()
+        self.stack_of_folders = self.folders
 
         while self.stack_of_folders != []:
             folder = self.stack_of_folders.pop()
@@ -67,25 +52,19 @@ class Folder:
         print('*************** Ja sam folder: ' + str(self.name))
         print('****Folders:')
         for folder in self.folders:
-            print(str(folder.name))
+            print(folder.name)
 
         print('****Files:')
         for file in self.files:
-            print(str(file.name))
+            print(file.name)
 
 
 class File:
-
-    root = None
-    name = None
-    href = None
 
     def __init__(self, rootFolder, name, href):
         self.root = rootFolder
         self.name = name
         self.href = href
-
-
 
 if __name__ == '__main__':
 
